edbafa1c7302db0099baacbeee8b9cf991f79bb9
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2026-09-18 01:21:12 +0900

    e2e: stabilize initialization on new machines
    Signed-off-by: Hiroki Hanada <hiroki-hanada@cybozu.co.jp>

--- internal/bpf/gen.go
+++ internal/bpf/gen.go
@@ -1,4 +1,4 @@
 package bpf
 
-//go:generate go tool bpf2go -cc clang -target bpfel SnatEgress ../../bpf/snat_egress.c -- -O2 -g -Wall -Werror -mcpu=v3 -I../../bpf/headers -I../../.dev-headers
-//go:generate go tool bpf2go -cc clang -target bpfel RevnatIngress ../../bpf/revnat_ingress.c -- -O2 -g -Wall -Werror -mcpu=v3 -I../../bpf/headers -I../../.dev-headers
+//go:generate go tool bpf2go -cc clang -target bpfel SnatEgress ../../bpf/snat_egress.c -- -O2 -g -Wall -Werror -Wno-missing-declarations -mcpu=v3 -I../../bpf/headers -I../../.dev-headers
+//go:generate go tool bpf2go -cc clang -target bpfel RevnatIngress ../../bpf/revnat_ingress.c -- -O2 -g -Wall -Werror -Wno-missing-declarations -mcpu=v3 -I../../bpf/headers -I../../.dev-headers
